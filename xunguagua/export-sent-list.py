f2e55710a0b66b9d359afd8b35f8e4d24e29df05
Domi <znfsky@hotmail.com>
2019-01-06 14:03:04 +0800

    Default Changelist

--- xunguagua/export-sent-list.py
+++ xunguagua/export-sent-list.py
@@ -1,10 +1,26 @@
 import json
 import time
 
+import os
 import requests
+
+_URL_ = "http://yun.xunguagua.com/member/?app=member&controller=list&type={type}&page={page}"
 
 print("input cookie:")
 cookie = input()
+
+print("choose type, 1-供求, 2-新闻")
+type_key = input()
+
+last_title = "_NONE_"
+if os.path.exists("./sent-list.csv"):
+    for line in open("./sent-list.csv", encoding="GBK"):
+        line = line.strip()
+        if not line:
+            continue
+        last_title = line.split(",")[0]
+if last_title != "_NONE_":
+    print("found last title: " + last_title)
 
 results = []
 page = 1
@@ -21,8 +37,7 @@ while page < 9999:
         "X-Requested-With": "XMLHttpRequest"
     }
 
-    resp = requests.get("http://yun.xunguagua.com/member/?app=member&controller=list&page={page}".format(page=page),
-                        headers=headers)
+    resp = requests.get(_URL_.format(type=type_key, page=page), headers=headers)
     if resp.status_code != 200:
         print("got status code: " + str(resp.status_code))
 
@@ -32,11 +47,16 @@ while page < 9999:
         break
 
     for sent_log in sent_list:
-        results.append(sent_log["title"].replace(",", "，") + "," + sent_log["url"])
+        title = sent_log["title"].replace(",", "，").strip()
+        if title == last_title:
+            print("encounter last title<{}>, stop.".format(title))
+            page = 10000
+            break
+        results.append(title + "," + sent_log["url"])
 
     print("page " + str(page) + " finished, current size: " + str(len(results)))
     time.sleep(2)
     page += 1
 
-with open("./sent-list.csv", encoding="GBK", mode="w") as f:
-    f.write("\n".join(results))
+with open("./sent-list.csv", encoding="GBK", mode="a") as f:
+    f.write("\n".join(reversed(results)))
